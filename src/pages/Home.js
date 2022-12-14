f166c0763433050b781041d7074423581bfce27e
Timothy Mei <timothymei3270@gmail.com>
2022-12-14 16:42:17 -0500

    addded softare engineer to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ const Home = () => {
   return (
     <div className="home">
       <h1>Timothy Mei</h1>
+      <h2>Software Engineer</h2>
       <p>Hello, welcome to my portfolio...</p>
     </div>
   )
